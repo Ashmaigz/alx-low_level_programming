ad02c261eb473c6a90f8f065ae6a4c49d8c9ac43
ashmaig <ibrahim.tarig2035@gmail.com>
2023-10-06 00:24:30 +0000

    e

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main input
+ * Description: a
+ *
+ * Return 0 (success)
+ *
+ */ 
+
 int main(void){
      puts("\"Programming is like building a multilingual puzzle");
        	return (0);
